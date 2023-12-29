de18bb468cafe0178676633b7b74ede3223983c1
hsadler <harrysadlermusic@gmail.com>
2023-12-30 00:29:55 +0100

    solve part 1 of day 11 2023

--- 2023/11/prog.py
+++ 2023/11/prog.py
@@ -3,13 +3,16 @@ sys.path.append('../..')
 import random
 import copy
 import uuid
+import itertools
 from dataclasses import dataclass
 
 from helpers import functions, grid
 
 # parse input
+INPUT_FILE = "input.txt"
+# INPUT_FILE = "test_input.txt"
 matrix: list[list[str]] = []
-with open("input.txt", "r") as f:
+with open(INPUT_FILE, "r") as f:
     lines = f.read().split("\n")
     for line in lines:
         matrix.append(list(line.strip()))
@@ -24,14 +27,14 @@ for y in range(g.get_height()):
     chars = [p.val for p in row]
     temp_matrix.append(chars)
     if "#" not in chars:
-        print("row", y)
+        # print("row", y)
         temp_matrix.append(chars)
 temp_matrix = functions.rotate_matrix(temp_matrix)
 expanded_matrix: list[list[str]] = []
 for y, chars in enumerate(temp_matrix):
     expanded_matrix.append(chars)
     if "#" not in chars:
-        print("row", y)
+        # print("row", y)
         expanded_matrix.append(chars)
 expanded_matrix = functions.rotate_matrix(expanded_matrix)
 g_expanded = grid.Grid.build(expanded_matrix)
@@ -41,4 +44,37 @@ for p in g_expanded.filter_points(g_expanded.get_points(), lambda p: p.val == ".
 for p in g_expanded.filter_points(g_expanded.get_points(), lambda p: p.val == "#"):
     p.color = grid.Color.YELLOW
 
-print(g_expanded.get_print_string())
+# color expanded space
+for y in range(g_expanded.get_height()):
+    chars = [p.val for p in g_expanded.get_row(y)]
+    if "#" not in chars:
+        for p in g_expanded.get_row(y):
+            p.color = grid.Color.GREEN
+for x in range(g_expanded.get_width()):
+    chars = [p.val for p in g_expanded.get_column(x)]
+    if "#" not in chars:
+        for p in g_expanded.get_column(x):
+            p.color = grid.Color.GREEN
+
+stars = [p for p in g_expanded.get_points() if p.val == "#"]
+star_pairs: set[tuple[grid.Point, grid.Point]] = set()
+# dedupe permutations
+for pairs in itertools.permutations(stars, 2):
+    pairs = list(pairs)
+    pairs.sort(key=lambda p: p.x)
+    pairs.sort(key=lambda p: p.y)
+    star_pairs.add(tuple(pairs))
+
+# calc distances between star pairs
+star_pair_distances = {}
+for star_pair in star_pairs:
+    p1, p2 = star_pair
+    distance = grid.Grid.manhattan_distance(p1, p2)
+    star_pair_distances[star_pair] = distance
+total_distance = 0
+for star_pair, distance in star_pair_distances.items():
+    total_distance += distance
+
+print(total_distance)
+
+# print(g_expanded.get_print_string())
--- helpers/grid.py
+++ helpers/grid.py
@@ -118,3 +118,6 @@ class Grid:
     @classmethod
     def filter_points(cls, points: list[Point], f: callable) -> list[Point]:
         return [p for p in points if f(p)]
+    @staticmethod
+    def manhattan_distance(p1: Point, p2: Point) -> int:
+        return abs(p1.x - p2.x) + abs(p1.y - p2.y)
